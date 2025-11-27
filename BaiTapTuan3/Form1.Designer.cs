bdf49e96f60985e776283c8ffe1f00d49af07ea6
Oanh-205 <ht.hoangoanh05@domain.com>
2025-11-27 12:42:26 +0700

    Them BaiTapTuan3

---
+++ BaiTapTuan3/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace BaiTapTuan3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblView = new System.Windows.Forms.Label();
+            this.lblFormat = new System.Windows.Forms.Label();
+            this.lblListView = new System.Windows.Forms.Label();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btnAddName = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblDetail = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblView
+            // 
+            this.lblView.AutoSize = true;
+            this.lblView.Location = new System.Drawing.Point(80, 20);
+            this.lblView.Name = "lblView";
+            this.lblView.Size = new System.Drawing.Size(36, 16);
+            this.lblView.TabIndex = 1;
+            this.lblView.Text = "View";
+            this.lblView.Click += new System.EventHandler(this.lblView_Click);
+            // 
+            // lblFormat
+            // 
+            this.lblFormat.AutoSize = true;
+            this.lblFormat.Location = new System.Drawing.Point(140, 20);
+            this.lblFormat.Name = "lblFormat";
+            this.lblFormat.Size = new System.Drawing.Size(49, 16);
+            this.lblFormat.TabIndex = 2;
+            this.lblFormat.Text = "Format";
+            this.lblFormat.Click += new System.EventHandler(this.lblFormat_Click);
+            // 
+            // lblListView
+            // 
+            this.lblListView.AutoSize = true;
+            this.lblListView.Location = new System.Drawing.Point(213, 20);
+            this.lblListView.Name = "lblListView";
+            this.lblListView.Size = new System.Drawing.Size(56, 16);
+            this.lblListView.TabIndex = 3;
+            this.lblListView.Text = "ListView";
+            this.lblListView.Click += new System.EventHandler(this.lblListView_Click);
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(24, 20);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(29, 16);
+            this.lblFile.TabIndex = 0;
+            this.lblFile.Text = "File";
+            this.lblFile.Click += new System.EventHandler(this.lblFile_Click);
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(472, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(297, 318);
+            this.label2.TabIndex = 6;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(511, 161);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(239, 22);
+            this.txtLastName.TabIndex = 7;
+            this.txtLastName.TextChanged += new System.EventHandler(this.txtLastName_TextChanged);
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(510, 130);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(72, 16);
+            this.lblLastName.TabIndex = 8;
+            this.lblLastName.Text = "Last Name";
+            this.lblLastName.Click += new System.EventHandler(this.lblLastName_Click);
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(510, 203);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(72, 16);
+            this.lblFirstName.TabIndex = 9;
+            this.lblFirstName.Text = "First Name";
+            this.lblFirstName.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(513, 236);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(239, 22);
+            this.txtFirstName.TabIndex = 10;
+            this.txtFirstName.TextChanged += new System.EventHandler(this.txtFirstName_TextChanged);
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(510, 276);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(46, 16);
+            this.lblPhone.TabIndex = 11;
+            this.lblPhone.Text = "Phone";
+            this.lblPhone.Click += new System.EventHandler(this.lblPhone_Click);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(513, 317);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(239, 22);
+            this.txtPhone.TabIndex = 12;
+            this.txtPhone.TextChanged += new System.EventHandler(this.txtPhone_TextChanged);
+            // 
+            // btnAddName
+            // 
+            this.btnAddName.Location = new System.Drawing.Point(629, 359);
+            this.btnAddName.Name = "btnAddName";
+            this.btnAddName.Size = new System.Drawing.Size(123, 32);
+            this.btnAddName.TabIndex = 13;
+            this.btnAddName.Text = "Add Name";
+            this.btnAddName.UseVisualStyleBackColor = true;
+            this.btnAddName.Click += new System.EventHandler(this.btnAddName_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.lblDetail);
+            this.panel1.Location = new System.Drawing.Point(26, 51);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(102, 31);
+            this.panel1.TabIndex = 14;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lblDetail
+            // 
+            this.lblDetail.AutoSize = true;
+            this.lblDetail.Location = new System.Drawing.Point(0, 4);
+            this.lblDetail.Name = "lblDetail";
+            this.lblDetail.Size = new System.Drawing.Size(42, 16);
+            this.lblDetail.TabIndex = 0;
+            this.lblDetail.Text = "Detail";
+            this.lblDetail.Click += new System.EventHandler(this.lblDetail_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(37, 146);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(421, 280);
+            this.listView1.TabIndex = 15;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(37, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 38);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Last Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(308, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 38);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Phone ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(174, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 38);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "First Name";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnAddName);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblListView);
+            this.Controls.Add(this.lblFormat);
+            this.Controls.Add(this.lblView);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblView;
+        private System.Windows.Forms.Label lblFormat;
+        private System.Windows.Forms.Label lblListView;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnAddName;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblDetail;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ BaiTapTuan3/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BaiTapTuan3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeListView();
+        }
+        // khởi tạo ListView 
+        private void InitializeListView()
+        {
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+
+            // Tạo 3 cột để hiển thị subitems
+            listView1.Columns.Add("", 100);  // Cột 1
+            listView1.Columns.Add("", 100);  // Cột 2
+            listView1.Columns.Add("", 120);  // Cột 3
+            listView1.HeaderStyle = ColumnHeaderStyle.None; // Ẩn header
+        }
+
+        // nút Add Name 
+        private void btnAddName_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtLastName.Text) ||
+                string.IsNullOrWhiteSpace(txtFirstName.Text) ||
+                string.IsNullOrWhiteSpace(txtPhone.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Tạo item và subitems (chia 3 cột)
+            ListViewItem item = new ListViewItem(txtLastName.Text);
+            item.SubItems.Add(txtFirstName.Text);
+            item.SubItems.Add(txtPhone.Text);
+
+            // Thêm item vào ListView
+            listView1.Items.Add(item);
+
+            // Xóa TextBox và focus lại
+            txtLastName.Clear();
+            txtFirstName.Clear();
+            txtPhone.Clear();
+            txtLastName.Focus();
+        }
+
+        // Click chọn item trong ListView 
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                txtLastName.Text = selectedItem.Text;
+                txtFirstName.Text = selectedItem.SubItems[1].Text;
+                txtPhone.Text = selectedItem.SubItems[2].Text;
+            }
+        }
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+            {
+                if (listView1.SelectedItems.Count > 0)
+                {
+                    // Lấy item được chọn
+                    ListViewItem selectedItem = listView1.SelectedItems[0];
+
+                    // Lấy dữ liệu từng cột (cột 0: Last Name, cột 1: First Name, cột 2: Phone)
+                    txtLastName.Text = selectedItem.Text;                     // Cột 0
+                    txtFirstName.Text = selectedItem.SubItems[1].Text;       // Cột 1
+                    txtPhone.Text = selectedItem.SubItems[2].Text;           // Cột 2
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.OldLace;
+        }
+
+        
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.OldLace;
+        }
+
+        private void label3_Click(object sender, EventArgs e) { }
+        private void label4_Click(object sender, EventArgs e) { }
+        private void lblPhone_Click(object sender, EventArgs e) { }
+        private void lblDetail_Click(object sender, EventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void txtLastName_TextChanged(object sender, EventArgs e) { }
+        private void lblLastName_Click(object sender, EventArgs e) { }
+        private void txtFirstName_TextChanged(object sender, EventArgs e) { }
+        private void txtPhone_TextChanged(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+        private void panel1_Paint(object sender, PaintEventArgs e) { }
+        private void lblFile_Click(object sender, EventArgs e) { }
+        private void lblView_Click(object sender, EventArgs e) { }
+        private void lblFormat_Click(object sender, EventArgs e) { }
+        private void lblListView_Click(object sender, EventArgs e) { }
+
+    }
+}
