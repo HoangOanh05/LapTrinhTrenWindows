6e290a67a96d041868bbd5e301070242b8b81fc5
Oanh-205 <ht.hoangoanh05@domain.com>
2026-01-06 10:11:23 +0700

    do an nhom 5

--- Nhom5/Program.cs
+++ Nhom5/Program.cs
@@ -16,7 +16,7 @@ namespace DoAn_Handmade
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_TrangChu());
+            Application.Run(new frm_Store());
         }
     }
 }
